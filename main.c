196bca8a8ab04b63ac2767c35681e094014c5497
Xinyu Li <lixinyu@loongson.cn>
2024-05-26 15:29:34 +0800

    fix mmap memory layout
    Signed-off-by: Xinyu Li <lixinyu@loongson.cn>

--- main.c
+++ main.c
@@ -615,7 +615,7 @@ static char* alloc_ram(uint64_t ram_size) {
     lsassert(start != MAP_FAILED);
     void *part1 = mmap(start, SZ_256M, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
     lsassert(part1 != MAP_FAILED);
-    void *part2 = mmap(start + SZ_2G, ram_size - SZ_256M, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+    void *part2 = mmap(start + SZ_2G + SZ_256M, ram_size - SZ_256M, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
     lsassert(part2 != MAP_FAILED);
     return part1;
 }
